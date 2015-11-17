a93e707495aed2211ff2b0a8ab7babfe62fa170d
Ben Nemec <bnemec@redhat.com>
2015-11-17 16:31:47 -0600

    Install pyghmi normally again
    pyghmi has released a version that unbreaks the Bmc class and
    supports setting the listen address that we need for single bmc
    functionality.  That means we don't have to pin to a forked repo
    anymore.

--- bin/install_openstackbmc.sh
+++ bin/install_openstackbmc.sh
@@ -5,7 +5,7 @@ yum -y update centos-release # required for rdo-release install to work
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum install -y https://rdo.fedorapeople.org/rdo-release.rpm
 yum install -y python-pip python-crypto os-net-config python-novaclient python-neutronclient git jq
-pip install --no-deps git+https://github.com/steveb/pyghmi.git@custom-bind-address
+pip install pyghmi
 
 cat <<EOF >/usr/local/bin/openstackbmc
 $openstackbmc_script
