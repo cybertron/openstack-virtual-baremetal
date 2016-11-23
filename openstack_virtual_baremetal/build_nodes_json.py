eaf23e8b77221d0d1cba84395c515ef55a45ec16
Derek Higgins <derekh@redhat.com>
2016-11-23 10:15:23 +0000

    Cache used image and flavors
    The image and flavor used for baremetal nodes is expected to be the
    same for each on. Don't request the objects for each one.

--- openstack_virtual_baremetal/build_nodes_json.py
+++ openstack_virtual_baremetal/build_nodes_json.py
@@ -126,14 +126,16 @@ def _build_nodes(nova, bmc_ports, bm_ports, provision_net):
 
     nodes = []
     bmc_bm_pairs = []
-
+    cache = {}
     for bmc_port, baremetal_port in zip(bmc_ports, bm_ports):
         baremetal = nova.servers.get(baremetal_port['device_id'])
         node = dict(node_template)
         node['pm_addr'] = bmc_port['fixed_ips'][0]['ip_address']
         bmc_bm_pairs.append((node['pm_addr'], baremetal.name))
         node['mac'] = [baremetal.addresses[provision_net][0]['OS-EXT-IPS-MAC:mac_addr']]
-        flavor = nova.flavors.get(baremetal.flavor['id'])
+        if not cache.get(baremetal.flavor['id']):
+            cache[baremetal.flavor['id']] = nova.flavors.get(baremetal.flavor['id'])
+        flavor = cache.get(baremetal.flavor['id'])
         node['cpu'] = flavor.vcpus
         node['memory'] = flavor.ram
         node['disk'] = flavor.disk
@@ -143,7 +145,9 @@ def _build_nodes(nova, bmc_ports, bm_ports, provision_net):
 
         # If a node has uefi firmware ironic needs to be aware of this, in nova
         # this is set using a image property called "hw_firmware_type"
-        image = nova.images.get(baremetal.image['id'])
+        if not cache.get(baremetal.image['id']):
+            cache[baremetal.image['id']] = nova.images.get(baremetal.image['id'])
+        image = cache.get(baremetal.image['id'])
         if image.metadata.get('hw_firmware_type') == 'uefi':
             node['capabilities'] = node['capabilities'] + ",boot_mode:uefi"
 
